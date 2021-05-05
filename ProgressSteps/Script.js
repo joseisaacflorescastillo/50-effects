7880acd21bf3e0fbcbddf702e529b56c407467c7
jose-isaac-fc <jose.isaac.fc@ibm.com>
2021-05-05 09:44:27 -0500

    Add steps

--- ProgressSteps/Script.js
+++ ProgressSteps/Script.js
@@ -1,6 +1,6 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
-const next = document.getElementById('next');
+const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
 
 let currentActiven = 1;
@@ -34,7 +34,16 @@ function update() {
     }
   });
 
-  const actives = document.querySelectorAll(".active");
+  const actives = document.querySelectorAll('.active')
 
-  console.log(actives.length, circles.length);
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+  if(currentActive === 1) {
+      prev.disabled = true
+  } else if(currentActive === circles.length) {
+      next.disabled = true
+  } else {
+      prev.disabled = false
+      next.disabled = false
+  }
 }
